b1ed265636214282d5f05247898e25852065977d
bayileyegn <bayileyegn@gmail.com>
2022-06-19 14:05:51 -0400

    completed multilsyer mapping

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,28 +1,25 @@
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/bayileyegnabate/13-mapping-earthquakes/main/majorAirports.json";
 
+// create the map object with center, zoom level and default layer
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    // layer: [streets]
+});
+
 // create the tile layer that will be the background of the map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/navigation-night-v1',
-    // tileSize: 512,
-    // zoomOffset: -1,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
     accessToken: API_KEY
-});
-
-// create the dark view tile layer
-let darklayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+}).addTo(map);
 
 // create a base layer tha holds both maps
-let baseMaps = {
-    Street: streets,
-    // Dark: darklayer
-};
+let baseMaps = {};
 
 let mapboxStyles = {
     street: 'streets-v11',
@@ -35,30 +32,15 @@ let mapboxStyles = {
     navigNight: 'navigation-night-v1'
 }
 
+// create the different layer styles
 Object.keys(mapboxStyles).forEach((style) => baseMaps[style] = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: `mapbox/${mapboxStyles[style]}`,
-    // tileSize: 512,
-    // zoomOffset: -1,
+    tileSize: 512,
+    zoomOffset: -1,
     accessToken: API_KEY
 }));
-
-// mapboxStyles.forEach((style) => baseMaps[style] = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: `mapbox/${style}`,
-//     // tileSize: 512,
-//     // zoomOffset: -1,
-//     accessToken: API_KEY
-// }))
-
-// create the map object with center, zoom level and default layer
-let map = L.map('mapid', {
-    center: [30, 30],
-    zoom: 2,
-    layer: [streets]
-});
 
 // pass the map layers to layers control abd add the layers control to map
 L.control.layers(baseMaps).addTo(map);
